2bd9f41f5f63addc50dda5c5f319f0fe1df79a84
Rob Champagne <68358594+LtDoc@users.noreply.github.com>
2024-06-05 19:50:34 -0400

    compats

--- WoE-Player-Map/script.js
+++ WoE-Player-Map/script.js
@@ -1,24 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCo9QPVrLCXS6li_kcTu3e-GOoiiwpHvLs",
-  authDomain: "woe-world.firebaseapp.com",
-  projectId: "woe-world",
-  storageBucket: "woe-world.appspot.com",
-  messagingSenderId: "706865712365",
-  appId: "1:706865712365:web:e080b1ef45b8d8b27190e4",
-  measurementId: "G-789BN2WECG"
+    apiKey: "AIzaSyCo9QPVrLCXS6li_kcTu3e-GOoiiwpHvLs",
+    authDomain: "woe-world.firebaseapp.com",
+    projectId: "woe-world",
+    storageBucket: "woe-world.appspot.com",
+    messagingSenderId: "706865712365",
+    appId: "1:706865712365:web:e080b1ef45b8d8b27190e4",
+    measurementId: "G-789BN2WECG"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 
 let isAdmin = false;
 let selectedMarker = null;
