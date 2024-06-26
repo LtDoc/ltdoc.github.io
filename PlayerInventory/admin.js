75e313707f2f15139bc079869df3ca8095aa659f
Rob Champagne <68358594+LtDoc@users.noreply.github.com>
2024-06-26 19:41:18 -0400

    ttgt

--- PlayerInventory/admin.js
+++ PlayerInventory/admin.js
@@ -1,22 +1,22 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCo9QPVrLCXS6li_kcTu3e-GOoiiwpHvLs",
+    authDomain: "woe-world.firebaseapp.com",
+    databaseURL: "https://woe-world-default-rtdb.firebaseio.com",
+    projectId: "woe-world",
+    storageBucket: "woe-world.appspot.com",
+    messagingSenderId: "706865712365",
+    appId: "1:706865712365:web:e080b1ef45b8d8b27190e4",
+    measurementId: "G-789BN2WECG"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
+const auth = firebase.auth();
+
 document.addEventListener('DOMContentLoaded', function() {
-    // Firebase Configuration
-    const firebaseConfig = {
-        apiKey: "AIzaSyCo9QPVrLCXS6li_kcTu3e-GOoiiwpHvLs",
-        authDomain: "woe-world.firebaseapp.com",
-        databaseURL: "https://woe-world-default-rtdb.firebaseio.com",
-        projectId: "woe-world",
-        storageBucket: "woe-world.appspot.com",
-        messagingSenderId: "706865712365",
-        appId: "1:706865712365:web:e080b1ef45b8d8b27190e4",
-        measurementId: "G-789BN2WECG"
-    };
-
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-
-    const db = firebase.database();
-    const auth = firebase.auth();
-
     document.getElementById('item-form').addEventListener('submit', e => {
         e.preventDefault();
 
