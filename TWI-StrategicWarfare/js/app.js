79b2257de3ccf3fad43be21f95405136779fb23d
DocTheMedic <darren.champagne@outlook.com>
2025-05-15 03:13:52 -0400

    refdsdrr

--- TWI-StrategicWarfare/js/app.js
+++
@@ -1,9 +0,0 @@
-import { checkDevIP } from './auth.js';
-import { initUI } from './ui.js';
-import { initMap } from './map.js';
-
-(async () => {
-  const isDev = await checkDevIP();
-  initMap(isDev);
-  initUI(isDev);
-})();
